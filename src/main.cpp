e16bc4a056671aa9511892a8336039ae1e463ac4
tvtdev <625747180@qq.com>
2019-02-17 17:50:49 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,11 @@ int main(int argc, char *argv[])
 	QBizManager _webSocket(0);
 	_webSocket.appendCookie(R"(__cfduid=dcd71745a264b4b002567ee878f8d5fb51548822274; cf_clearance=bd7f4edb2841c1b337042555e3c52314520a1ba2-1548822284-10800-150; PHPSESSID=mo9e0bvquhfmuff8ek5j1652m0; language=8ad2f0e6c0f6cd9f5387ac8328a11816d806564808d2448f50e9d0a5a4aa0e81a%3A2%3A%7Bi%3A0%3Bs%3A8%3A%22language%22%3Bi%3A1%3Bs%3A5%3A%22en-US%22%3B%7D; _csrf=7eba384496df32a523989360e367857c44f625c56262f194a5954658d4299cf5a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22%AE%EA%D5%93%5D%B5%29%F6%B6%5B%5Dcym%1A%A1%18%7FBx%AA%13x%EB%A0%7E%40%0B%CE%DB%EE%EF%22%3B%7D; language_id=en-US; _ga=GA1.2.1990910632.1548822294; _gid=GA1.2.1473442867.1548822294; _gat=1; _u=383910; _s=5335ca5640656fc7b098a5cfd0274825; websocket_secure_key=057c4ce788183cc26bdf222bcec9ab9c)");;
 	
+	
+	Twitter twitter(0);
 	_webSocket.do_cf_clearance();
 	
-	_webSocket.testmail();
+	twitter.testmail();
 
 	
 	qDebug() << "bqDebug test";
