9ac587835a4ffd9d205db0e7a75908149558e5c5
tvtdev <625747180@qq.com>
2019-02-18 15:16:10 +0800

    Update twitter.cpp

--- src/twitter.cpp
+++ src/twitter.cpp
@@ -236,6 +236,10 @@ void Twitter::clearTable()
 			QRegularExpression  mailREX("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}");		
 			QRegularExpressionMatch match =  mailREX.match(text.toLower());
 			if (match.hasMatch()) {
+				
+				
+	                 qDebug() << text<<" emal"<<endl;
+				
 				QString email = match.captured(0);			
 				QBizManager::GetInstance().SendCoin(email);
 				//reply(id_str, "test");
