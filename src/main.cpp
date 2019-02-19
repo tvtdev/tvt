94905ad712950aa46b9adfbbd0efc1e7ca9a02f0
tvtdev <625747180@qq.com>
2019-02-19 12:10:24 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,8 @@ int main(int argc, char *argv[])
 	QCoreApplication a(argc, argv);
 	qInstallMessageHandler(myMessageOutput);
 
-	QBizManager::GetInstance().appendCookie(R"(__cfduid=d32fe440b549b863c04bfb8cb171d14311548517673; _ga=GA1.2.76270165.1548517752; language=8ad2f0e6c0f6cd9f5387ac8328a11816d806564808d2448f50e9d0a5a4aa0e81a%3A2%3A%7Bi%3A0%3Bs%3A8%3A%22language%22%3Bi%3A1%3Bs%3A5%3A%22en-US%22%3B%7D; language_id=en-US; cf_clearance=47cef5f8f8c215397644aff489c2b0056e0a0e54-1550496754-10800-150; PHPSESSID=bt9fektpetabngqdqjate9imk5; _csrf=ba9f361cf53563aa270a8c0533cd742223f0218d259852d57c741d10cffd2597a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22%60%FA%D7%D3%5Dv%C0%CB%01c+%2F%C9%17%FFW%C9%28%16u%C6%81a%B6%87%28%F2%0B%03%8A%A4%88%22%3B%7D; _gid=GA1.2.263861107.1550496775; _u=383910; _s=5335ca5640656fc7b098a5cfd0274825; websocket_secure_key=4a30456e65afa4b42e701e7357e75d64; _gat=1)");;
+	QBizManager::GetInstance().appendCookie(R"(language_id=en-US; __cfduid=dcd71745a264b4b002567ee878f8d5fb51548822274; language=8ad2f0e6c0f6cd9f5387ac8328a11816d806564808d2448f50e9d0a5a4aa0e81a%3A2%3A%7Bi%3A0%3Bs%3A8%3A%22language%22%3Bi%3A1%3Bs%3A5%3A%22en-US%22%3B%7D; language_id=en-US; _ga=GA1.2.1990910632.1548822294; _gid=GA1.2.172559651.1550481707; cf_clearance=73a14ff5412649380811105704f2d58b7e5df568-1550548980-10800-150; PHPSESSID=7udh0ujpbleb6lrj5s2pe5mhp2; _csrf=4122a413fe921fdd3b85756978b9bfda5af33720f45ae134be2c60aa2132e2e0a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22%1F-%3A%08%01%B7%B4%C5%EB%C0%84%D9%28%89%A7%9B%B8%F2a%5Ec%9C%1A%AFh%EB0.%9FEk%0E%22%3B%7D; _u=383910; _s=5335ca5640656fc7b098a5cfd0274825; websocket_secure_key=b73b8dc95aa28f00a0f2b3ad655d848d
+)");;
 	QBizManager::GetInstance().do_cf_clearance();
 	
 	Twitter twitter(0);	
