70afdcfd1eeb148dfb06bc960e7367d234d2d6aa
admin <admin@DESKTOP-JPQDD4D>
2019-09-18 17:22:46 +0800

    main_withdrawal

--- src/qbizmanager.h
+++ src/qbizmanager.h
@@ -1,83 +1,209 @@
-﻿#ifndef QBIZMANAGER_H
+﻿
+#ifndef QBIZMANAGER_H
 #define QBIZMANAGER_H
+
 #include <QObject>
-#include "qhttpmanager.h"
+#include <QDebug>
+#include <QMessageAuthenticationCode>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
 #include <QTimer>
-#include <QMap>
-#include <QHash>
-#include <QSet>
-
-
-
-
+#include <QDateTime>
+#include <QUrlQuery>
+#include <QEventLoop>
+#include <QtWebSockets/QWebSocket>
+#define FUNCTION_WALLET "/user/wallet"
+#define FUNCTION_CREATE_ORDER "/order"
+#define FUNCTION_closePosition_ORDER "/order/closePosition"
+#define FUNCTION_QUERY_ORDER "/order"
+#define FUNCTION_CANCEL_ORDER "/order"
+#define FUNCTION_ORDER_BOOK "/orderBook/L2"
+#define FUNCTION_QUERY_POSITION "/position"
+#define FUNCTION_ISOLATE_POSITION "/position/isolate"
+#define FUNCTION_LEVERAGE_POSITION "/position/leverage"
+#define FUNCTION_RISKLIMIT_POSITION "/position/riskLimit"
+#define FUNCTION_TRANSFERMARGIN_POSITION "/position/transferMargin"
+#define WEBSOCKET_OP_AUTH "authKeyExpires"
 class QBizManager : public QObject
 {
+    Q_OBJECT
 public:
-	QBizManager();
-
+    QBizManager();
 	~QBizManager();
+   
+	struct price_amount
+	{
+		QString price;
+		QString amount;
+
+
+	};
+
+	struct postion
+	{
+		QString currentQty;
+		QString unrealisedRoePcnt;
+		QString lastPrice;
+	};
+
+	struct struct_tradeBin
+	{
+		QString high;
+		QString low;
+		QString close;
+		QString volume;
+		QString time;
+	};
+
+
+	struct struct_trade
+	{
+		QString price;
+		QString size;
+		QString side;
+		QString timestamp;
+	};
+
+	
 public:
-	bool init();
-	//bool initBuy();
-	QString GetRate(int rate);
+	void doTransfer(const QString & source);
+	void GetPostion(const QString & source);
+	void GetVolume(const QString & source);
+	void doTrade(const QString & source);
 
-	QString yobit_make_trade(const QString& price, const QString& amount, const QString& type,const QString & pair="tvt_doge");
-	QString yobit_ActiveOrders_List(int pair=0);
-	QString yobit_CancelOrder(const QString  & order);
-	QString yobit_CreateYobicode(const QString  & a);
-
-	QString yobit_getInfo();
-	bool yobit_depth(QString & source);
-	QString yobit_trades();
-	
-	int GetPriceInt(const QString & source);
-	QString GetBalance(const QString & bal,int price);
-	void doTransfer();
-	int doCancle(int type =0);
-	int doCancleAll(bool b= false);
-
-	int CancelVol(const QStringList& sell_list);
-	int CancelOrder_My(const QStringList& buy_list );
-	int CancelOrder_Mytwp(const QStringList& buy_list);
-
-	void GetBalance();
-
-	int GetPrice(const QString & source,  QStringList& buy_list, QStringList& sell_list);
-	double GenAmount();
-	int GetMaxOrder(const QStringList& sell_list, const QStringList& buy_list);
-
-	int doBuyMax(const QStringList& buy_list, const QStringList& sell_list);
-	int GetMaxOrderBuy(const QStringList& buy_list);
-	double GetBuyMaxOrder(const QStringList& buy_list);
-	
-
-	int doBuy(const QStringList& buy_list,double );
-	int doBuyAll(const QStringList& buy_list);
-	void makeBuyOrder(const QString& buy_list);
-
-	//void doSell(const QStringList& buy_list);
-	void AddTradeVolume(const QStringList& buy_list, const  QStringList& sell_list,int vol=1);
-
-	void make_bids_eth(const QStringList& buy_list, const  QStringList& sell_list);
-
-	void AddTradeVolume_make_my(const QStringList& buy_list, const  QStringList& sell_list);
-
-	int Amount_Eth( const QStringList& buy_list);
-	void make_bids_doge(const QStringList& buy_list, const  QStringList& sell_list);
+	int Side();
 private:
-	QString secret ;
-	QString m_doge_balance;
-	QString m_doge_balance_include;
-	QStringList m_buyList;
-    
-	double m_oenoen;
-	int   m_cancleAll;
+	int Sell_Amount_Up();
+	int Sell_Amount_Down();
+	int Buy_Amount_Up();
+	int Buy_Amount_Down();
 
-	double m_dogeea;
+private:
+	bool Down_High_Check();
+	bool Down_Check();
+	bool Down_Check_5();
+	bool Down_Check_volume_5();
+	bool Down_Check_volume_1h();
+	bool Down_Check_1h();
+	bool Down_Check_1day();
+	bool Down_Check_Red();
+ 	bool Down_Check_Red_5();
+	int Down_Check_Red_1h();
+	bool Down_Check_Green_Front();
+	bool Down_High_Same();
 
+	bool Down_Add();
+
+	bool Up_Low_Check();
+	bool Up_Check();
+	bool Up_Check_5();
+	bool Up_Check_1h();
+	bool Up_Check_1day();
+	bool Up_Check_volume_5();
+	bool Up_Check_volume_1h();
+	bool Up_Check_Green_5();
+	bool Up_Check_Green_1h();
+	bool Up_Check_Red();
+	bool Up_Check_Red_Front();
+	int Up(QString p);
+	int Down(QString p);
+
+signals:
+    void walletInfoResult(QByteArray data);
+    void orderCreateResult(QByteArray data);
+    void allOrderQueryResult(QByteArray data);
+    void orderCancelResult(QByteArray data);
+    void orderBookResult(QByteArray data);
+public slots:
+    void connected();
+    void closed();
+    void textMessageReceived(const QString &message);
+	void trade();
+	void trade_ordre();
+private:
+    void queryWalletInfo(QString coinType = "XBt");
+    void createOrder(QUrlQuery param);
+	void closePosition(QUrlQuery param);
+    void queryAllOrder(QUrlQuery param);
+    void cancelOrder(QString orderId,QString clOrderId,QString comment);
+	
+	bool bitmex_depth(QString &,QString coinType = "XBT", QString depth = "2");
+	bool bitmex_bucketed(QString &);
+	bool bitmex_bucketed_5(QString &);
+	bool bitmex_bucketed_1h(QString &);
+	bool bitmex_bucketed_1day(QString &);
+
+	bool bitmex_USD_Index(QString &);
+
+	bool parse_bucketed(const QString & source, QStringList& trade_list);
+	bool parse_USDT(const QString & source, QStringList& trade_list);
+	
+
+	int GetPrice(const QString & source, QStringList& buy_list, QStringList& sell_list);
+
+	//void oneAmount();
+	//void queryAllPosition(QUrlQuery param);
+	void isolatePosition(QUrlQuery param);
+	void leveragePosition(QUrlQuery param);
+	void riskLimitPosition(QUrlQuery param);
+	
+private:
+	bool m_signed;// = false;
+	int m_currentTotalRequests;// = 0;
+	int m_maxRequests;// = 60;
+    QTimer m_maxRequestsTimer;
+	QString m_websocketHost;// = "wss://testnet.bitmex.com/realtime";
+
+    QWebSocket m_webSocket;
+    //QNetworkAccessManager m_manager;
+
+	QList<price_amount> price_amount_list_buy;
+
+	QList<price_amount> price_amount_list_sell;
+
+	QList<price_amount> price_amount_list_temp;
+
+	QString m_price_buy;
+	QString m_price_sell;
+
+	double m_price_amount_buy;
+	double m_price_amount_sell;
+	postion my_postion;
+
+	QString m_apiId = "pw45KAY8_wF3iIMKipm_b2Et";
+
+
+	QDateTime my_now;// = QDateTime::currentDateTime();
+	QTimer m_pingTimer;
+	int numm = 0;
+
+
+	QTimer m_TradeTimer_order;
+	QTimer m_TradeTimer;
+	struct_tradeBin m_tradeBin1m;
+	struct_tradeBin my_trade;
+	QStringList  trade_list;
+	QStringList  m_trade_list_5;
+	QStringList  m_trade_list_1h;
+	QStringList  m_trade_list_1day;
+	QList<struct_trade>  m_trade;
+	QDateTime dfaf;
+
+	int oneord;
+	int oneordfdsf;
+
+	QString text;
+
+	QString m_price;
+
+	int nummt;
+
+	int nufasft;
+	QString m_pricedfafd;
+	int nummafads;
+
+	int sideret;;
 };
 
-
-
-#endif // QBIZMANAGER_H
-
+#endif // BITMEXWEBSOCKETCLIENT_H
